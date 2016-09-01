6483e62577d287f307dfe79c3507f832cfade832
Michael Ducey <michaeljducey@gmail.com>
2016-09-01 12:05:50 -0500

    saving progress, api is visible

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
 	def index
 		@message = Message.all.order(created_at: :desc)
 		@new_message = Message.new
-		# @message_time =  Time.now.strftime("%s") - @message.created_at.strftime("%s")
+
 		@time = Time.now.strftime("%s")
 	end
 
