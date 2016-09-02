4ace0c7aa3ed6c1a4ef2dd2ad7b36525328be614
Michael Ducey <michaeljducey@gmail.com>
2016-09-02 15:54:26 -0500

    work done to make it look better, still might need to move methods?

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,10 @@
 class Message < ActiveRecord::Base
+
+	# def time_since_signing(entry)
+	# 	@current_time = Time.now.strftime("%s").to_i
+	# 	@time_difference = (@current_time - entry/86400)
+
+	# 	return @time_difference
+	# end
+
 end
